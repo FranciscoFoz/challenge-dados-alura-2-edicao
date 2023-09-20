6f15ad8ac04afe1d654ba9947b0831c3f5e66c5c
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-09-20 07:15:23 -0300

    Continuando desenvolvimento da interface do app

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from joblib import load
 import pandas as pd
 
+pd.options.display.max_columns = 999
+
 st.set_page_config(
     layout='wide',
     page_title='predicao-churn-novexus',
@@ -17,7 +19,8 @@ expander_servicos = st.expander("Serviços")
 expander_contrato = st.expander("Contrato")
 
 dict_respostas = {}
-lista_campos = load('src/features/features.joblib')
+lista_campos = load('src/features/lista_campos.pkl')
+
 
 perguntas = {
     'idoso': 'O cliente é idoso?',
@@ -37,7 +40,6 @@ perguntas = {
     'fatura_mensal': 'Qual o valor médio da fatura mensal do cliente?',
 }
 
--------------------------------------------------CORRIGIR ERRO-------------------------------------------------
 with expander_pessoal:
     for campo in ['idoso', 'tem_conjuge', 'tem_dependentes']:
         dict_respostas[campo] = st.selectbox(perguntas[campo], ['Sim', 'Não'])
@@ -49,7 +51,44 @@ with expander_servicos:
         dict_respostas[campo] = st.selectbox(perguntas[campo], lista_campos[campo])
 
 with expander_contrato:
-    for campo in ['meses_de_contrato', 'tipo_de_contrato', 'recebimento_de_fatura_online', 'forma_de_pagamento',
-                  'fatura_mensal']:
+    for campo in ['tipo_de_contrato', 'recebimento_de_fatura_online', 'forma_de_pagamento']:
         dict_respostas[campo] = st.selectbox(perguntas[campo], lista_campos[campo])
 
+    dict_respostas['meses_de_contrato'] = st.slider(perguntas['meses_de_contrato'],
+                                                    help='Pode-se mover a barra usando as setas do teclado',
+                                                    min_value=0, max_value=360, step=1)
+    dict_respostas['fatura_mensal'] = st.slider(perguntas['fatura_mensal'],
+                                                help='Pode-se mover a barra usando as setas do teclado',
+                                                min_value=0, max_value=200, step=1)
+        
+''''''
+respostas = pd.DataFrame([dict_respostas])
+
+respostas_1 =respostas.replace({'Não':0,'Sim':1})
+
+variaveis_numericas = respostas_1[['meses_de_contrato','fatura_mensal']]
+
+variaveis_binarias = respostas_1[['idoso','tem_conjuge','tem_dependentes']]
+
+variaveis_multiplas = respostas_1[['possui_servico_internet','possui_servico_seguranca_online',
+                                   'possui_servico_backup_online','possui_servico_protecao_dispositivo',
+                                   'possui_servico_suporte_tecnico','possui_TV_a_cabo',
+                                   'possui_TV_streaming','tipo_de_contrato','forma_de_pagamento']]
+
+variaveis_multiplas_normalizadas = pd.get_dummies(variaveis_multiplas,dtype=int)
+
+df_final = pd.concat([variaveis_binarias,variaveis_numericas,variaveis_multiplas_normalizadas],axis=1)
+
+
+modelo = load('src/models/modelo_churn_novexus_1.joblib')
+features = load('src/features/features.joblib')
+
+df_final = df_final.reindex(columns=features, fill_value=0)
+df_final
+
+--------------------------------------VERIFICAR MODELO ------------------------------------
+print(modelo.predict(df_final))
+
+-------------------------------------REFATORAR TRANSFORMAÇÃO (FUNÇÕES)----------------------------------------
+-------------------------------------BOTÃO--------------------------------------------------------------------
+-------------------------------------PROBABILIDADE------------------------------------------------------------
